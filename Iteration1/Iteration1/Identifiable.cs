92ef66a53764dcd2c6c55e6c192017777228803f
uuuummmmm <51980230+uuuummmmm@users.noreply.github.com>
2019-09-16 21:05:08 +1000

    Add files via upload

---
+++ Iteration1/Iteration1/Identifiable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration1
+{
+        class Identifiable
+        {
+            private List<string> _identifiers;
+
+            public readonly string FirstId;
+
+            public Identifiable(List<string> idents)
+            {
+                _identifiers = idents;
+
+                if (_identifiers.Count() < 1)
+                {
+                    FirstId = "";
+                }
+                else
+                {
+                    FirstId = _identifiers[0];
+                }
+            }
+
+            public bool AreYou(string id)
+            {
+                id = id.ToLower();
+                foreach (string temp_id in _identifiers)
+                {
+                    if (id == temp_id)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            public void AddIdentifier(string id)
+            {
+                id = id.ToLower();
+
+                _identifiers.Add(id);
+            }
+        }
+
+    }
---
+++ Iteration1/Iteration1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration1
+{
+   
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test 1");
+            Identifiable test = new Identifiable(new List<string> { "bob", "fred" });
+
+            if (test.AreYou("bob"))
+            {
+                Console.WriteLine("Passed Test 1");
+            }
+
+            Console.WriteLine("Test 2");
+
+            if (!test.AreYou("omg"))
+            {
+                Console.WriteLine("Passed Test 2");
+            }
+
+            Console.WriteLine("Test 3");
+
+            if (test.AreYou("BOB"))
+            {
+                Console.WriteLine("Passed Test 3");
+            }
+
+            Console.WriteLine("Test 4");
+
+            if (test.FirstId == "bob")
+            {
+                Console.WriteLine("Passed Test 4");
+            }
+
+            Console.WriteLine("Test 5");
+
+            test.AddIdentifier("wilma");
+
+            if (test.AreYou("wilma"))
+            {
+                Console.WriteLine("Passed Test 5");
+            }
+
+            Console.WriteLine("Press Enter to Exit");
+            Console.ReadLine();
+        }
+    }
+}
